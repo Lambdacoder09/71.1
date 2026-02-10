b080028ba9f32893a7612952353a4910d02b581e
zayedknight007 <zayedknight007@gmail.com>
2026-02-10 19:01:17 +0000

    Changes before Firebase Studio auto-run

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Routes, Route, useLocation } from "react-router-dom";
 
 import "./App.css";
 
-import Header from "./assets/components/HeaderBar.jsx";
-import Footer from "./assets/components/Footer.jsx";
+import Header from "./components/layout/Header.jsx";
+import Footer from "./components/layout/Footer.jsx";
 
-import GalleryPage from "./assets/components/GalleryPage.jsx";
-import CartPage from "./assets/components/Cart/CartPage.jsx";
-import DescPage from "./assets/components/DescPage.jsx";
+import GalleryPage from "./pages/GalleryPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import DescPage from "./pages/DescriptionPage.jsx";
 
 function App() {
   const location = useLocation();
